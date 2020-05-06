3da07815649e75c0129af0b8fdffc5b9dc3b6939
Tharaka Romesh <tharakaromesh@gmail.com>
2020-05-06 13:19:31 +0530

    #1 #2 changes

--- .grenrc.js
+++ .grenrc.js
@@ -1,12 +1,19 @@
 module.exports = {
   dataSource: "milestones",
   prefix: "",
-  groupBy: "label",
   milestoneMatch: "Release {{tag_name}}",
   changelogFilename: "CHANGELOG.md",
   ignoreIssuesWith: ["duplicate", "wontfix", "invalid", "help wanted"],
   template: {
-    issue: "- [{{text}}]({{url}}) {{name}}",
+    commit: ({ message, url, author, name }) =>
+      `- [${message}](${url}) - ${author ? `@${author}` : name}`,
+    issue: "- {{labels}} {{name}} [{{text}}]({{url}})",
+    label: "[**{{label}}**]",
+    noLabel: "closed",
+    group: "\n#### {{heading}}\n",
+    changelogTitle: "# Changelog\n\n",
+    release: "## {{release}} ({{date}})\n{{body}}",
+    releaseSeparator: "\n---\n\n",
   },
   groupBy: {
     "Enhancements:": ["enhancement", "internal"],
--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,11 +1,4 @@
 # Changelog
 
-## v0.17.1 (12/12/2019)
-
-#### Enhancements:
-
-- [#1](https://github.com/TRomesh/release/issues/21) [feature request] add gren
-
-#### Bug Fixes:
-
-- [#2](https://github.com/TRomesh/release/issues/2) fix issue in .grenrc
+## v3.13 (06/05/2020)
+*No changelog for this release.*
