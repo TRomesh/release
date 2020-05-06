cdf9d78bb33062a41972a59eb01111c98e9b735c
Tharaka Romesh <tharakaromesh@gmail.com>
2020-05-06 16:42:29 +0530

    asasasasas

--- .grenrc.js
+++ .grenrc.js
@@ -10,10 +10,24 @@ module.exports = {
     issue: "- {{labels}} {{name}} [{{text}}]({{url}})",
     label: "[**{{label}}**]",
     noLabel: "closed",
-    group: "\n#### {{heading}}\n",
     changelogTitle: "# Changelog\n\n",
     release: "## {{release}} ({{date}})\n{{body}}",
     releaseSeparator: "\n---\n\n",
+    group: function (placeholders) {
+      var icon = "🙈";
+      if (placeholders.heading == "Enhancements") {
+        icon = "🚀";
+      } else if (placeholders.heading == "Bug Fixes" || "Bugs") {
+        icon = "🐛";
+      } else if (placeholders.heading == "Documentation") {
+        icon = "📚";
+      } else if (placeholders.heading == "No user affected") {
+        icon = "🙈";
+      } else if (placeholders.heading == "Technical Tasks") {
+        icon = "⚙️";
+      }
+      return "\n#### " + icon + " " + placeholders.heading + "\n";
+    },
   },
   groupBy: {
     "Enhancements:": ["enhancement", "internal"],
--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,4 +1,14 @@
 # Changelog
 
+## v3.132 (06/05/2020)
+*No changelog for this release.*
+
+---
+
 ## v3.13 (06/05/2020)
+*No changelog for this release.*
+
+---
+
+## v3.12 (06/05/2020)
 *No changelog for this release.*
